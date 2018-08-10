0fec1def7654bd41051e2b306790de3ee26f07fd
Tarun Jangra <tarun@izap.in>
2018-08-10 16:37:25 +0530

    My angular next setsp

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gi-servers',
+  template: `
+  <gi-server></gi-server>
+  <gi-server></gi-server>
+  <gi-server></gi-server>
+  <gi-server></gi-server>
+  <gi-server></gi-server>
+  <gi-server></gi-server>
+  <gi-server></gi-server>
+  <gi-server></gi-server>
+  <gi-server></gi-server>
+  <gi-server></gi-server>
+  <gi-server></gi-server>
+  <gi-server></gi-server>
+  `,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
