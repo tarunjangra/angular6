61f867a1927f02df9734093a7a6bf4b72c7b4b6a
Tarun Jangra <tarun@izap.in>
2018-08-12 02:05:12 +0530

    shopping project layout added

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+  selector: 'gi-header'
+})
+
+export class HeaderComponent {
+
+}
