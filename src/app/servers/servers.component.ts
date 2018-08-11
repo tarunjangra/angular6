3a39c19b0b1ef0fdf9c4019b1714dee538a6b9a5
Tarun Jangra <tarun@izap.in>
2018-08-12 00:54:02 +0530

    ngClass implementaiton

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -25,6 +25,7 @@ export class ServersComponent implements OnInit {
   onCreateServer(){
     this.serverCreationStatus = "Server was created! Name is " + this.serverName;
     this.availableServers.push(this.serverName);
+    this.serverCreated = true;
   }
 
   onUpdateServerName(event: Event){
