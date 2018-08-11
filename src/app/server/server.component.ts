5930d323a1d3500cded4980f936f891dd53e6ec9
Tarun Jangra <tarun@izap.in>
2018-08-12 00:50:53 +0530

    ngStyle implementation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,14 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'gi-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+    .online {
+      color: white;
+    }
+    `]
 })
 
 export class ServerComponent{
     serverId: number = 10;
     serverStatus: string = "offline";
 
-    getServerStatus(){
-        return this.serverStatus;
+    constructor(){
+      this.serverStatus = Math.random() > 0.5?'offline':'online';
     }
+
+    getServerStatus(){
+      return this.serverStatus;
+    }
+
+    getColorByStatus(){
+      return this.getServerStatus() === 'offline'?'red':'green';
+    }
+
+
 }
