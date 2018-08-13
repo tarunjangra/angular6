58633353aca8fc485f735422e6044a17cffbfe52
Tarun Jangra <tarun@izap.in>
2018-08-14 02:00:12 +0530

    recipe implementation with parent,child,child components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter();
   recipes: Recipe[] = [
-    new Recipe('A test recipe','A test','https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
-    new Recipe('A test recipe','A test','https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
+    new Recipe('Tarun recipe','A test for tarun','https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
+    new Recipe('deepak Recipe','A test for deepak','https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
     new Recipe('A test recipe','A test','https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
     new Recipe('A test recipe','A test','https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
     new Recipe('A test recipe','A test','https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
@@ -22,6 +22,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelecte(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
