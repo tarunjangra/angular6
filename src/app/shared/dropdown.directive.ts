6977413ccc95564bb943201f48c104e345f61693
Tarun Jangra <tarun@izap.in>
2018-08-14 13:15:28 +0530

    added directive with better toggling implementation

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,18 +1,20 @@
-import { Directive, HostListener, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer2, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[giDropdown]'
 })
 export class DropdownDirective {
 
-  constructor(
-    private element: ElementRef,
-    private renderer: Renderer2
-  ) { }
-  @HostListener('mouseenter') mouseOver(){
-    this.renderer.addClass(this.element.nativeElement,'open');
-  }
-  @HostListener('mouseleave') mouseLeave(){
-    this.renderer.removeClass(this.element.nativeElement,'open');
+  @HostBinding('class.open') isOpen = false; 
+  constructor() { }
+  // @HostListener('mouseenter') mouseOver(){
+  //   this.renderer.addClass(this.element.nativeElement,'open');
+  // }
+  // @HostListener('mouseleave') mouseLeave(){
+  //   this.renderer.removeClass(this.element.nativeElement,'open');
+  // }
+
+  @HostListener('click') toggleDropdown(){
+    this.isOpen = !this.isOpen;
   }
 }
